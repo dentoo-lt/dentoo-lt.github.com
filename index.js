dd56e0275898949469b59b7614edff6f5444c1ef
gae-22 <kaneryo1014@gmail.com>
2022-05-17 06:30:02 +0900

    TEST

---
+++ index.js
@@ -0,0 +1,50 @@
+var makeshadow = false;
+var delayx = 0;
+var delayy = 0;
+var delayvelocity = 0;
+var mousex = -1;
+var mousey = -1;
+
+function makeShadow(x, y){
+    var logoitemtext = document.getElementsByClassName("logoitemtext");
+    for(i=0;i<logoitemtext.length;i++){
+        logoitemtext[i].style.textShadow = String(x) + "px " + String(y) + "px 0px gray";
+    }
+}
+
+window.setInterval(function(){
+    if(makeshadow){
+        makeShadowWithMouse();
+    }
+}, 60);
+
+window.setTimeout(function(){
+    makeShadow(0, -10);
+    delayx = window.innerWidth / 2;
+    delayy = window.innerHeight / 2 + 40;
+    makeshadow = true;
+}, 3400);
+
+function makeShadowWithMouse() {
+    if(mousex >= 0 && mousey >= 0){
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        delayvelocity = Math.sqrt((mousex-delayx)*(mousex-delayx)+(mousey-delayy)*(mousey-delayy)) / 10000;
+        delayx = (delayx + mousex * delayvelocity) / (1 + delayvelocity);
+        delayy = (delayy + mousey * delayvelocity) / (1 + delayvelocity);
+        makeShadow((width / 2 - delayx) / 4, (height / 2 - delayy) / 4);
+    }
+}
+
+
+function init()
+    {
+        window.onmousemove = changeMouseXY;
+        function changeMouseXY(event) {
+            if(makeshadow){
+                event = event || window.event; 
+                mousex = event.clientX;
+                mousey = event.clientY;
+            }
+        }
+    }
