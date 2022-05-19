298b76d0e6c7e8caffbb64f1d1ed07752d7f595b
gae-22 <kaneryo1014@gmail.com>
2022-05-20 07:54:33 +0900

    fadein

--- public/index.js
+++ public/index.js
@@ -80,23 +80,16 @@ $(document).ready(() => {
 })
 
 /* fadein */
-<script>
-(window.onload = function() {
+window.onload=function(){
+    var scroll = document.querySelectorAll('.up');
 
-  // フェードイン処理
-jQuery(window).scroll(function (){
-    jQuery(".fade").each(function(){
-    var winheight = jQuery(window).height();
-    var posi = jQuery(this).offset().top;
-    var scroll = jQuery(window).scrollTop();
-    if (scroll + winheight > posi){
-        jQuery(this).addClass("fadein");
-    } else {
-        //　スクロールで画面上部に戻った際に要素を非表示にしたい場合は、下記の行のコメントを外し有効にしてください。
-        //jQuery(this).removeClass("fadein");
+    var Animation = function() {
+    for(var i = 0; i < scroll.length; i++) {
+    var triggerMargin = 80;
+    if (window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
+    scroll[i].classList.add('show');
     }
-    });
-});
-
-})();
-</script>
+    }
+    }
+    window.addEventListener('scroll', Animation);
+}
