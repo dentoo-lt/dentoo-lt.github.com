dc31fef41193283388c5142067a8af82fcb2fe80
gae-22 <gae-22@users.noreply.github.com>
2022-10-19 03:26:50 +0000

    deploy: d61ddc267a424f5e2774af6e52e97bdcc53b81bf

---
+++ guideline.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+    fadein();
+};
+
+/* fadein */
+function fadein() {
+    var scroll = document.querySelectorAll(".up");
+
+    var Animation = function () {
+        for (var i = 0; i < scroll.length; i++) {
+            var triggerMargin = 80;
+            if (
+                window.innerHeight >
+                scroll[i].getBoundingClientRect().top + triggerMargin
+            ) {
+                scroll[i].classList.add("show");
+            }
+        }
+    };
+    window.addEventListener("scroll", Animation);
+    Animation();
+};
+
+// ボタン表示
+// jQuery(function($){
+//     $(window).on('scroll', function(){
+//       var obj = document.getElementsByClassName("menu-btn")[0];
+//       if ($(window).scrollTop() > -1) {
+//         obj.style.visibility = "visible";
+//       } else {
+//         obj.style.visibility = "hidden";
+//       }
+//     });
+//   });
+  jQuery(function($){
+    $(window).on('scroll', function(){
+      var obj = document.getElementsByClassName("pagetop")[0];
+      if ($(window).scrollTop() > 300) {
+        obj.style.visibility = "visible";
+      } else {
+        obj.style.visibility = "hidden";
+      }
+    });
+  });
+
+  // ボタン色
+  function touchMenubtn(){
+  document.getElementById("menu_btn").addEventListener("touchstart",function(){
+    document.getElementById("menu_btn").style.backgroundColor = "red";
+  })
+  document.getElementsByClassName("page_top_btn")[0].addEventListener("touchstart", function(){
+    document.getElementsByClassName("page_top_btn")[0].style.backgroundColor = "red";
+  })
+  document.getElementById("menu_btn").addEventListener("touchend", function(){
+    document.getElementById("menu_btn").style.backgroundColor = "black";
+  })
+  document.getElementsByClassName("page_top_btn")[0].addEventListener("touchend",function(){
+    document.getElementsByClassName("page_top_btn")[0].style.backgroundColor = "black";
+  })
+  }
+
+  // メニューで指定位置に飛ぶ
+  function scrollJump (position){
+    document.getElementById(position).scrollIntoView(true)
+  }
+
+  //メニューの外側をクリックしてメニューを閉じる
+  function deletemenu(){
+    document.getElementById("menu-btn-check").checked = false;
+  }
